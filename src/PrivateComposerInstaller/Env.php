e0b5e53cca1ad9a701f7d6ae10f62fc2c153f393
Graham Campbell <graham@alt-three.com>
2019-10-20 18:19:08 +0100

    Fallback to env file

--- src/PrivateComposerInstaller/Env.php
+++ src/PrivateComposerInstaller/Env.php
@@ -3,37 +3,42 @@
 namespace FFraenz\PrivateComposerInstaller;
 
 use Dotenv\Environment\Adapter\ArrayAdapter;
+use Dotenv\Environment\Adapter\PutenvAdapter;
 use Dotenv\Environment\DotenvFactory;
 use Dotenv\Loader;
 use FFraenz\PrivateComposerInstaller\Exception\MissingEnvException;
 
 class Env
 {
-    protected $adapter;
+    protected $arrayAdapter;
+    protected $getenvAdapter;
     protected $path;
     protected $loaded = false;
 
     public function __construct($path)
     {
-        $this->adapter = new ArrayAdapter();
+        $this->arrayAdapter = new ArrayAdapter();
+        $this->getenvAdapter = new PutenvAdapter();
         $this->path = $path;
     }
 
     public function load()
     {
         $this->loaded = true;
-        $loader = new Loader([$this->path], new DotenvFactory([$this->adapter]));
+        $loader = new Loader([$this->path], new DotenvFactory([$this->arrayAdapter]));
         $loader->load();
     }
 
     public function get($key)
     {
-        if (!$this->loaded) {
-            $this->load();
-        }
+        $this->getenvAdapter->get($key)->getOrCall(function() use ($key) {
+            if (!$this->loaded) {
+                $this->load();
+            }
 
-        return $this->adapter->get($key)->getOrCall(function() use ($key) {
-            throw new MissingEnvException($key);
+            return $this->arrayAdapter->get($key)->getOrCall(function() use ($key) {
+                throw new MissingEnvException($key);
+            });
         });
     }
 }
