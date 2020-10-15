0ce3d383c70e323f78bd41957a0a75ae8b4ce63a
Viktor Szépe <viktor@szepe.net>
2020-10-15 21:45:46 +0200

    Fix unit tests

--- test/PrivateComposerInstaller/PluginTest.php
+++ test/PrivateComposerInstaller/PluginTest.php
@@ -21,7 +21,7 @@ use PHPUnit\Framework\TestCase;
 
 class PluginTest extends TestCase
 {
-    protected function tearDown()
+    protected function tearDown(): void
     {
         // Unset environment variables
         putenv('KEY_FOO');
